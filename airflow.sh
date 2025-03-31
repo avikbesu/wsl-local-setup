e8b6440de0f0b3982642af874922b60d01662ed0
Avik Mandal <avik.burdwan@gmail.com>
2025-03-31 16:37:31 +0530

    added for airflow

---
+++ airflow.sh
@@ -0,0 +1,44 @@
+
+# add airflow charts repo
+helm repo add apache-airflow https://airflow.apache.org 
+helm repo update
+
+# how to download charts
+helm pull apache-airflow/airflow --untar --destination ./chart
+
+# install airflow
+
+kubectl apply -f airflow/dags_pv.yaml -n airflow
+kubectl apply -f airflow/dags_pvc.yaml -n airflow
+
+# Install Airflow
+helm install airflow apache-airflow/airflow \
+  --namespace airflow --create-namespace \
+  --set executor=LocalExecutor \
+  --set dags.persistence.enabled=true \
+  --set dags.persistence.existingClaim=dags-pvc \
+  --set dags.gitSync.enabled=false
+
+# port forwarding not working from k9s
+kubectl port-forward airflow-webserver-6cbbf4b88f-vz957 8090:8080 -n airflow
+
+#uninstall airflow
+helm uninstall airflow --namespace airflow
+# check the pods
+kubectl get pods  
+
+# check the services
+kubectl get services 
+
+# check the deployments
+kubectl get deployments
+
+# check the events
+kubectl get events
+
+# check the nodes
+kubectl get nodes
+
+# check the config
+kubectl config view
+
